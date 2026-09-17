24c6485239e15d5ec37625a488e7bf0d24becf04
Lauratro <lauratronchin@hotmail.it>
2026-09-17 11:43:25 +0200

    Update import

--- src/app/language-switcher/language-switcher.tsx
+++ src/app/language-switcher/language-switcher.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import type { ResumeLocale } from "@/data/resume-data";
+import type { ResumeLocale } from "./../../data/resume-data";
 import Link from "next/link";
-import { useEffect } from "react";
 
 export function LanguageSwitcher({ locale }: { locale: ResumeLocale }) {
   return (
